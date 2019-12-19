4fcdd0b8e8e4f49e16296918bad9211ed9e9ab82
solayunus <solayunus19@gmail.com>
2019-12-19 17:19:05 +0100

    Updates

---
+++ precache-manifest.0ca2a7c645e0449f6d8f00d3c3a7b396.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fce54d41931997924fb54ec1d2a0b9",
+    "url": "solayunus.github.io/myapp/index.html"
+  },
+  {
+    "revision": "3ad793a35f293cbd085f",
+    "url": "solayunus.github.io/myapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cc56ca433aa9add4c52e",
+    "url": "solayunus.github.io/myapp/static/js/2.36767d31.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "solayunus.github.io/myapp/static/js/2.36767d31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ad793a35f293cbd085f",
+    "url": "solayunus.github.io/myapp/static/js/main.5d10c015.chunk.js"
+  },
+  {
+    "revision": "02cca1d84575b53ec6f0",
+    "url": "solayunus.github.io/myapp/static/js/runtime-main.405ec9fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "solayunus.github.io/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
